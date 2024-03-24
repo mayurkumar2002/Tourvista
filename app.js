069482d6375f4946d05bd6860592900fa033eaed
mayurkumar2002 <mk8995454@gmail.com>
2024-03-24 23:09:48 +0530

    Background Image Updated

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ const reviewRoutes=require('./routes/reviews');
 const userRoutes=require('./routes/users');
 const MongoDBStore = require("connect-mongo");
 
-const dbUrl = process.env.DB_URL;
-// const dbUrl = 'mongodb://127.0.0.1:27017/Tourvista';
+// const dbUrl = process.env.DB_URL;
+const dbUrl = 'mongodb://127.0.0.1:27017/Tourvista';
 // 'mongodb://127.0.0.1:27017/Tourvista'
 mongoose.connect(dbUrl);
 const db=mongoose.connection;
